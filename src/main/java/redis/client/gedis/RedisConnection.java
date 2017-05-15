e803188a406300bd12d6aadeaac9c2d97eedbfb9
nischal kumar <nischal.k@pw-c02mxrwjfh00.fkipl.flipkart.com>
2017-05-15 20:15:07 +0530

    first commit

---
+++ src/main/java/redis/client/gedis/RedisConnection.java
@@ -0,0 +1,13 @@
+package redis.client.gedis;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * Created by nischal.k on 15/05/17.
+ */
+public class RedisConnection implements Closeable {
+    public void close() throws IOException {
+
+    }
+}
